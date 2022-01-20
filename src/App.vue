30227917d7ef67f5b0b3eb45b273d6ac29e4bf11
HikkiSlava <wowkulaka555@gmail.com>
2022-01-20 02:35:03 +0200

    app struct v2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <router-view/>
+  <div id='app'>
+    <header class="head">
+      <router-link to="/"><img alt="App logo" src="./assets/logo.png"></router-link>
+    </header>
+    <main>
+      <nav class="nav-menu">
+        <router-link to="/roll" class="active">Роллы</router-link>
+        <router-link to="/soup" class="active">Супы</router-link>
+        <router-link to="/salad" class="active">Салаты</router-link>
+        <router-link to="/panasia" class="active">Паназия</router-link>
+        <router-link to="/discount" class="active">Акции</router-link>
+        <router-link to="/desert-and-drink" class="active">Десерты и напитки</router-link>
+      </nav>
+      <div class="content"></div>
+    </main>
+    <footer class="footer"></footer>
+  </div>
 </template>
 
 <script>
-import Home from '../src/views/Home.vue'
 
 export default {
- name: 'App',
- components: {
-    Home
- }
+ name: 'App'
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Roll from '../views/Roll.vue'
-import InHome from '../components/InHome'
-import Soup from '../views/Soup.vue'
-import Salad from '../views/Salad.vue'
-import PanAsian from '../views/PanAsian.vue'
-import Discount from '../views/Discount.vue'
-import DesertDrink from '../views/DesertDrink.vue'
-
+import App from '../App.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/roll',
-    name: 'roll',
-    component: Roll
-  },
-  {
-    path: '/inhome',
-    name: '/inhome',
-    component: InHome
-  },
-  {
-    path: '/soup',
-    name: 'soup',
-    component: Soup
-  },
-  {
-    path: '/salad',
-    name: 'salad',
-    component: Salad
-  },
-  {
-    path: '/panasia',
-    name: 'panasia',
-    component: PanAsian
-  },
-  {
-    path: '/discount',
-    name: 'discount',
-    component: Discount
-  },
-  {
-    path: '/desert-and-drink',
-    name: 'desert-and-drink',
-    component: DesertDrink
+    name: 'app',
+    component: App 
   }
 ]
 
