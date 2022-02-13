bdb2deebb8f53c2432598d5ef5b297cc360211be
Ярослав <wowkulaka555@gmail.com>
2022-02-13 15:51:49 +0200

    carved slider

--- src/views/v-main.vue
+++ src/views/v-main.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="v-main-content"> 
-      <Slider
-        :carousel_data="SliderItems"
-      />
+      <Slider/>
   </div>
 </template>
 
@@ -12,22 +10,13 @@ import Slider from '../components/v-carousel.vue'
 export default {
     name: 'Main',
     components: {
-      Slider
-    },
-    data () {
-      return {
-        SliderItems: [
-          {id: 1, name: 'img1', img: 'desert.png'},
-          {id: 2, name: 'img2', img: 'panasia.png'},
-          {id: 3, name: 'img3', img: 'roll.png'},
-          {id: 4, name: 'img4', img: 'soup.png'},
-          {id: 5, name: 'img5', img: 'Sushi.png'}
-        ]
-      }
+      Slider,
     }
 }
 </script>
 
 <style>
-
+#v-main-content {
+  position: relative;
+}
 </style>
